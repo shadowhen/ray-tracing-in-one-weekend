3374ce45cab78780cdbb692ac7686b22158540e7
Henry <henry@henrynhan.com>
2025-06-11 19:42:20 -0500

    Add multiple objects for ray tracing

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -1,36 +1,20 @@
-#include "color.h"
-#include "vec3.h"
-#include "ray.h"
+#include "rtweekend.h"
 
-#include <iostream>
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
-double hit_sphere(const point3& center, const double& radius, const ray& r)
+color ray_color(const ray& r, const hittable& world)
 {
-	vec3 oc = center - r.origin();
-	double a = r.direction().length_squared();
-	double h = dot(r.direction(), oc);
-	double c = oc.length_squared() - radius * radius;
-	double discriminant = h * h - a * c;
-
-	if (discriminant < 0)
+	hit_record rec;
+	if (world.hit(r, interval(0, infinity), rec))
 	{
-		return -1.0;
-	}
-	return (h - std::sqrt(discriminant) ) / a;
-}
-
-color ray_color(const ray& r)
-{
-	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-	if (t > 0.0)
-	{
-		vec3 n = unit_vector(r.at(t) - vec3(0, 0, -1));
-		return 0.5 * color(n.x() + 1, n.y() + 1, n.z() + 1);
+		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
-	return (1.0 - a)*color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main()
@@ -43,6 +27,12 @@ int main()
 	// Calculate the height, ensure that it's at least 1
 	int image_height = static_cast<int>(image_width / aspect_ratio);
 	image_height = image_height < 1 ? 1 : image_height;
+
+	// World
+
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	// Camera
 
@@ -80,7 +70,7 @@ int main()
 			auto ray_direction = pixel_center - camera_center;
 			ray r(camera_center, ray_direction);
 
-			color pixel_color = ray_color(r);
+			color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
@@ -89,4 +79,3 @@ int main()
 
 	return 0;
 }
-
--- raytracer/ray.h
+++ raytracer/ray.h
@@ -3,22 +3,24 @@
 
 #include "vec3.h"
 
-class ray {
+class ray
+{
 public:
-    ray() {}
+	ray() = default;
 
-    ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+	ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
 
-    const point3& origin() const { return orig; }
-    const vec3& direction() const { return dir; }
+	const point3& origin() const { return orig; }
+	const vec3& direction() const { return dir; }
 
-    point3 at(double t) const {
-        return orig + t * dir;
-    }
+	point3 at(double t) const
+	{
+		return orig + t * dir;
+	}
 
 private:
-    point3 orig;
-    vec3 dir;
+	point3 orig;
+	vec3 dir;
 };
 
 #endif

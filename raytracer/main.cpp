5cd0b4b9b36acaae01b834281b59360a1b981524
Henry <henry@henrynhan.com>
2025-06-14 14:58:46 -0500

    Add positionable camera

--- raytracer/main.cpp
+++ raytracer/main.cpp
@@ -17,10 +17,10 @@ int main()
 	auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
 	world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-	world.add(make_shared<sphere>(point3(0.0,    0.0, -1.2),   0.5, material_center));
-	world.add(make_shared<sphere>(point3(-1.0,   0.0, -1.0),   0.5, material_left));
-	world.add(make_shared<sphere>(point3(-1.0,   0.0, -1.0),   0.4, material_bubble));
-	world.add(make_shared<sphere>(point3( 1.0,   0.0, -1.0),   0.5, material_right));
+	world.add(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+	world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
+	world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
 	camera cam;
 
@@ -28,6 +28,11 @@ int main()
 	cam.image_width = 400;
 	cam.samples_per_pixel = 100;
 	cam.max_depth = 50;
+
+	cam.vfov = 20;
+	cam.lookfrom = point3(-2, 2, 1);
+	cam.lookat = point3(0, 0, -1);
+	cam.vup = vec3(0, 1, 0);
 
 	cam.render(world);
 

975f86142899a7a8694236f160430eb64ffceece
Henry <henry@henrynhan.com>
2025-06-14 14:36:30 -0500

    Add dielectrics

--- raytracer/material.h
+++ raytracer/material.h
@@ -57,4 +57,51 @@ private:
 	double fuzz;
 };
 
+class dielectric : public material
+{
+public:
+	dielectric(const double& reflection_index) : reflection_index(reflection_index) {}
+
+	bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override
+	{
+		attenuation = color(1.0, 1.0, 1.0);
+		double ri = rec.front_face ? 1.0 / reflection_index : reflection_index;
+
+		vec3 unit_direction = unit_vector(r_in.direction());
+
+		double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+		double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+		bool cannot_refract = ri * sin_theta > 1.0;
+		vec3 direction;
+
+		if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+		{
+			// Must reflect
+			direction = reflect(unit_direction, rec.normal);
+		}
+		else
+		{
+			// Can refract
+			direction = refract(unit_direction, rec.normal, ri);
+		}
+
+		scattered = ray(rec.p, direction);
+		return true;
+	}
+
+private:
+	// Refractive index in vacuum or air, or the ratio of the material's refractive index over
+	// the refractive index of the enclosing media
+	double reflection_index;
+
+	static double reflectance(const double& cosine, const double& refraction_index)
+	{
+		// Use Schlick's approximation for reflectance.
+		auto r0 = (1 - refraction_index) / (1 + refraction_index);
+		r0 = r0 * r0;
+		return r0 + (1 - r0) * std::pow(1 - cosine, 5);
+	}
+};
+
 #endif
